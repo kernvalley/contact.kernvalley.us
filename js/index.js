8ceb273482165182c93ec454575fa56ef44b2691
Chris Zuber <shgysk8zer0@gmail.com>
2020-12-22 11:37:47 -0800

    Add Google Analytics
    Also fixes typo in preloading `<toast-message>` stylesheet

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ import 'https://cdn.kernvalley.us/components/pwa/install.js';
 import 'https://cdn.kernvalley.us/components/github/user.js';
 import 'https://cdn.kernvalley.us/components/share-target.js';
 import 'https://cdn.kernvalley.us/components/app/list-button.js';
-import { toggleClass } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
-import { importGa } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
+import { toggleClass, ready, on } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import { importGa, externalHandler, mailtoHandler, telHandler } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
 import { GA } from './consts.js';
 
 toggleClass(document.documentElement, {
@@ -18,5 +18,27 @@ toggleClass(document.documentElement, {
 });
 
 if (typeof GA === 'string') {
-	requestIdleCallback(() => importGa(GA).catch(console.error));
+	requestIdleCallback(() => {
+		importGa(GA).then(async ({ ga }) => {
+			if (ga instanceof Function) {
+				ga('create', GA, 'auto');
+				ga('set', 'transport', 'beacon');
+				ga('send', 'pageview');
+				on(document.forms.contact, 'submit', () => {
+					ga('send', {
+						hitType: 'event',
+						eventCategory: 'submit',
+						eventAction: 'submit',
+						eventLabel: 'Contact Form',
+					});
+				}, { passive: true, capture: true });
+			}
+
+			await ready();
+
+			on('a[rel~="external"]', '.click', externalHandler, { passive: true, capture: true });
+			on('a[href^="tel:"]', 'click', telHandler, { passive: true, capture: true });
+			on('a[href^="mailto:"]', 'click', mailtoHandler, { passive: true, capture: true });
+		});
+	});
 }
