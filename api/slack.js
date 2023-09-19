ed84ef73b86dcbbc19f815dd8b3b9af750716699
Chris Zuber <shgysk8zer0@gmail.com>
2023-09-19 12:09:58 -0700

    Check for `endsWith` in suffix test

--- api/slack.js
+++ api/slack.js
@@ -43,7 +43,7 @@ if (typeof process.env.BASE_URL === 'string') {
 
 function allowedOrigin(url) {
 	const origin = new URL(url).origin;
-	return ALLOWED_ORIGINS.includes(origin) || ALLOWED_DOMAIN_SUFFIXES.includes(origin);
+	return ALLOWED_ORIGINS.includes(origin) || ALLOWED_DOMAIN_SUFFIXES.some(suff => origin.endsWith(suff));
 }
 
 export async function handler(event) {
