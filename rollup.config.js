f5a937e12a8c292e476dc0e0e6a3158adf404ef6
Chris Zuber <shgysk8zer0@gmail.com>
2026-03-01 09:12:44 -0800

    Update dependencies & importmap

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import { getConfig } from '@shgysk8zer0/js-utils/rollup';
 import { rollupImport, rollupImportMeta } from '@shgysk8zer0/rollup-import';
+import { importmap } from '@shgysk8zer0/importmap';
 
 export default getConfig('./js/index.js', {
 	plugins: [
-		rollupImport('./importmap.json'),
+		rollupImport(importmap),
 		rollupImportMeta({ baseURL: 'https://contact.kernvalley.us/' }),
 	],
 	format: 'iife',
